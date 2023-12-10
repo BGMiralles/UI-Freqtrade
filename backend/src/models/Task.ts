32d48feb766ef444d774044844ebcb189700a987
Borja <pintocorreas@gmail.com>
2023-12-10 20:36:05 +0100

    feat: backend template

---
+++ backend/src/models/Task.ts
@@ -0,0 +1,44 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, ManyToMany, JoinTable } from "typeorm"
+import { User } from "./User"
+
+@Entity("tasks")
+export class Task extends BaseEntity{
+  @PrimaryGeneratedColumn()
+  id!: number
+
+  @Column()
+  title!: string
+
+  @Column()
+  description!: string
+
+  @Column()
+  status!: boolean
+
+  @Column()
+  user_id!: number
+  
+  @Column()
+  created_at!: Date
+  
+  @Column()
+  updated_at!: Date
+
+  @ManyToOne(() => User, (user) => user.tasks)
+  @JoinColumn({ name: "user_id"})
+  user!: User;
+
+  @ManyToMany(() => User)
+  @JoinTable({
+    name: "task_user",
+    joinColumn: {
+      name: "task_id",
+      referencedColumnName: "id",
+    },
+    inverseJoinColumn:{
+      name: "user_id",
+      referencedColumnName: "id"
+    }
+  })
+  taskUsers!: User[]
+}
---
+++ backend/src/routes/tasksRoutes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { createTask, getAllTasksByUserId, getTaskByUserId, updateTaskById } from "../controllers/tasksController";
+import { auth } from "../middlewares/auth";
+
+const router = Router()
+
+router.post('/', auth, createTask)
+router.get('/', auth, getAllTasksByUserId)
+router.get('/:id', auth, getTaskByUserId)
+router.put('/:id', auth, updateTaskById)
+// router.get('/assigned', auth, getTasksAssignedbyUser)
+
+export { router }
