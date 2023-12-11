b18b86f04bb6c54fcb60cb2f5fdd732111a13588
Borja <pintocorreas@gmail.com>
2023-12-11 11:30:37 +0100

    feat: created role model

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -4,6 +4,8 @@ import 'dotenv/config'
 
 import { CreateUsersTable1698050881658 } from "./migration/1698050881658-create-users-table"
 import { User } from "./models/User"
+import { RoleFkAtUsers1702290088419 } from "./migration/1702290088419-role_fk_at_users"
+import { Role } from "./models/Role"
 
 export const AppDataSource = new DataSource({
   type: "mysql",
@@ -12,9 +14,10 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [User],
+  entities: [User, Role],
   migrations: [
-    CreateUsersTable1698050881658
+    CreateUsersTable1698050881658,
+    RoleFkAtUsers1702290088419
   ],
   synchronize: false,
   logging: false,
---
+++ backend/src/migration/1702290088419-role_fk_at_users.ts
@@ -0,0 +1,33 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export class RoleFkAtUsers1702290088419 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "roles",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "role",
+                        type: "enum",
+                        enum: ["user", "admin", "super_admin"],
+                        default: '"user"',
+                      }
+                ],
+            }),
+            true
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("roles");
+    }
+
+}
