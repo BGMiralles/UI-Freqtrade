a2608047581d9b99527cb72e2ac44087439376c2
Borja <pintocorreas@gmail.com>
2023-12-14 16:59:14 +0100

    feat: Add user management functionality

--- backend/src/controllers/superAdminController.ts
+++ backend/src/controllers/superAdminController.ts
@@ -1,50 +1,157 @@
-import { Request, Response } from 'express';
-import { User } from '../models/User';
+import { Request, Response } from "express";
+import { User } from "../models/User";
+import { TimeFrame } from "../models/TimeFrame";
+import { TechnicalResource } from "../models/TechnicalResource";
 
 const getAllUsers = async (req: Request, res: Response) => {
     try {
         const users = await User.find();
-        return res.json(
-            {
-              success: true,
-              message: "users retrieved",
-              data: users
-            }
-          )
+        return res.json({
+            success: true,
+            message: "users retrieved",
+            data: users,
+        });
     } catch (error) {
-        return res.json(
-            {
-              success: false,
-              message: "users cant be retrieved",
-              error: error
-            }
-          )
-        }
+        return res.json({
+            success: false,
+            message: "users cant be retrieved",
+            error: error,
+        });
     }
+};
 
-    const deleteUser = async (req: Request, res: Response) => {
-        try {
-          const id = req.body.id;
-      
-          const userToRemove = await User.findOneBy({
+const deleteUser = async (req: Request, res: Response) => {
+    try {
+        const id = req.body.id;
+
+        if (!id) {
+            return res.json({
+                success: false,
+                message: "Invalid user ID",
+            });
+        }
+
+        const userToRemove = await User.findOneBy({
             id,
-          });
-          if (userToRemove) {
-            await User.remove(userToRemove);
-          }
-      
-          return res.json({
+        });
+        if (!userToRemove) {
+            return res.json({
+                success: false,
+                message: "User not found",
+            });
+        }
+
+        await User.remove(userToRemove);
+
+        return res.json({
             success: true,
             message: "User deleted",
             data: userToRemove,
-          });
-        } catch (error) {
-          return res.json({
+        });
+    } catch (error) {
+        return res.json({
             success: false,
-            message: "User cant by deleted",
+            message: "User cant be deleted",
             error: error,
-          });
-        }
-      };
+        });
+    }
+};
 
-export { getAllUsers, deleteUser }
+const updateUserRole = async (req: Request, res: Response) => {
+    try {
+        const { id, role } = req.body;
+
+        if (!id || !role) {
+            return res.json({
+                success: false,
+                message: "Invalid user ID or role",
+            });
+        }
+
+        const userToUpdate = await User.findOneBy({
+            id,
+        });
+        if (!userToUpdate) {
+            return res.json({
+                success: false,
+                message: "User not found",
+            });
+        }
+
+        userToUpdate.role = role;
+        await userToUpdate.save();
+
+        return res.json({
+            success: true,
+            message: "User role updated",
+            data: userToUpdate,
+        });
+    } catch (error) {
+        return res.json({
+            success: false,
+            message: "User role cant be updated",
+            error: error,
+        });
+    }
+};
+
+const createTimeFrame = async (req: Request, res: Response) => {
+    try {
+        const { time_frame } = req.body;
+
+        if (!time_frame) {
+            return res.json({
+                success: false,
+                message: "Invalid time frame",
+            });
+        }
+
+        const createTimeFrame = await TimeFrame.create({
+            time_frame,
+        });
+
+        return res.json({
+            success: true,
+            message: "Time frame created",
+            data: createTimeFrame,
+        });
+    } catch (error) {
+        return res.json({
+            success: false,
+            message: "Time frame cant be created",
+            error: error,
+        });
+    }
+};
+
+const createTechnicalResource = async (req: Request, res: Response) => {
+    try {
+        const { name, description } = req.body;
+
+        if (!name || !description) {
+            return res.json({
+                success: false,
+                message: "Invalid technical resource",
+            });
+        }
+
+        const createTechnicalResource = await TechnicalResource.create({
+            name,
+            description,
+        });
+
+        return res.json({
+            success: true,
+            message: "Technical resource created",
+            data: createTechnicalResource,
+        });
+    } catch (error) {
+        return res.json({
+            success: false,
+            message: "Technical resource cant be created",
+            error: error,
+        });
+    }
+}
+
+export { getAllUsers, deleteUser, updateUserRole, createTimeFrame, createTechnicalResource };
--- backend/src/routes/superAdminRoutes.ts
+++ backend/src/routes/superAdminRoutes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { auth } from "../middlewares/auth";
 import { isSuperAdmin } from "../middlewares/isSuperAdmin";
-import { deleteUser, getAllUsers } from "../controllers/superAdminController";
+import { createTechnicalResource, createTimeFrame, deleteUser, getAllUsers, updateUserRole } from "../controllers/superAdminController";
 
 const router = Router()
 
 router.get('/all', auth, isSuperAdmin, getAllUsers)
 router.delete('/delete', auth, isSuperAdmin, deleteUser)
+router.put('/update', auth, isSuperAdmin, updateUserRole)
+router.get('/timeframe', auth, isSuperAdmin, createTimeFrame)
+router.get('/technicalresource', auth, isSuperAdmin, createTechnicalResource)
 
 export { router }
